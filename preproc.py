484067620ec4c36c5f8ae7d8d634244c344e661f
Florentin Hennecker <florentin.hennecker@gmail.com>
2017-02-01 21:21:22 +0100

    [add] RNN output

--- preproc.py
+++ preproc.py
@@ -1,12 +1,11 @@
 import json
 
-jsonfiles = [str(i) for i in range(2009,2018)]
+jsonfiles = [str(i) for i in range(2015,2018)]
 outfile = open('data/cleaned.txt', 'w')
 
 for jsonfile in jsonfiles:
     data = json.loads(open('data/'+jsonfile+'.json', 'r').read())
     for tweet in data:
-        print(tweet['text'])
         outfile.write(tweet['text']+'\n')
 
 
--- twomp.py
+++ twomp.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
 
 dataset = list(open('data/cleaned.txt', 'r').readlines())
 def get_batch(batch_size):
@@ -8,17 +9,25 @@ def get_batch(batch_size):
 
 
 class Trumpinator():
-    def __init__(self, batch_size):
+    def __init__(self, batch_size, seqlen, nchars):
         
         self.input = tf.placeholder(tf.int32, [batch_size, 140])
         self.lengths = tf.placeholder(tf.int32, [batch_size])
+        embedded_inputs = tf.one_hot(self.input, nchars)
         
         cell = tf.nn.rnn_cell.BasicLSTMCell(100)
         cell = tf.nn.rnn_cell.MultiRNNCell([cell] * 2)
 
         self.init_state = cell.zero_state(batch_size, tf.float32)
 
+        output, self.output_state = tf.nn.dynamic_rnn(
+                cell,
+                embedded_inputs,
+                initial_state=self.init_state)
+
+        self.output = tf.nn.softmax(slim.fully_connected(output, nchars))
+
 
 if __name__ == "__main__":
-    t = Trumpinator(32)
+    t = Trumpinator(32, 20, 61)
 
